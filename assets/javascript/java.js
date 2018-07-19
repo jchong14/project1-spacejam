d1df35821ff37782c2197dab70342e5c0da7ba77
Ji Chong <ji_no8@yahoo.com>
2018-07-18 20:47:59 -0700

    saving as a checkpoint

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -35,6 +35,7 @@ for (var i = 0; i < computerOption.length; i++) {
   } else {
     answerArray.push("_");
   }
+  // console.log(computerOption);
   // console.log(answerArray);
 }
 
@@ -49,9 +50,15 @@ function startTimer() {
 
   if (seconds <= 0 || guessesLeft == 0) {
     $("#mainBox").addClass("hide");
-
-    clearInterval(timer);
+    $("#gifs-appear-here img")
+      .css("border", "0px solid yellowgreen")
+      .animate({
+        borderWidth: "10px",
+        borderColor: "#f37736"
+      });
+    // clearInterval(timer);
     $("#highScore").removeClass("hide");
+    $("#score").removeClass("hide");
   }
 }
 
@@ -73,6 +80,9 @@ function playGame(ev) {
     correctGuesses.push(userGuess);
     $("#yourguesses").text(guessesSoFar);
     $("#correctguesses").text(correctGuesses);
+    var dis = display();
+    console.log(dis);
+    $("#wordToGuess").text(dis);
   } else if (
     computerOption.indexOf(userGuess) == -1 &&
     guessesSoFar.indexOf(userGuess) == -1
@@ -82,23 +92,51 @@ function playGame(ev) {
     $("#yourguesses").text(guessesSoFar);
     $("#guessesleft").text(guessesLeft);
   }
+
+  var winGif = dis;
+
+  // Constructing a URL to search Giphy for the name of the person who said the quote
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    winGif +
+    "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  // Performing our AJAX GET request
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.data[0].images.fixed_height.url);
+    var gifDiv = $("<div class= 'item'>");
+    var gifImage = $("<img>");
+
+    gifImage.attr("src", response.data[0].images.fixed_height.url);
+    gifDiv.append(gifImage);
+
+    $("#gifs-appear-here")
+      .empty()
+      .prepend(gifDiv);
+  });
 }
 
 document.onkeyup = playGame;
-var disp = "";
 
 function display() {
+  var disp = "";
   for (var h = 0; h < computerOption.length; h++) {
     if (correctGuesses.indexOf(computerOption[h]) != -1) {
       disp = disp + computerOption[h];
     } else {
       disp = disp + " _ ";
     }
-    var element = $("#wordToGuess").text(disp);
   }
-  $("#wordToGuess").push(element);
+
   return disp;
 }
+
+//make an array called letters and check if what key they pressed is inside letters.
+// $("#wordToGuess").push(element);
 
 //if(ev.keyCode >=97 && ev.keyCode <=122) {
 //     $('#letters-guessed').append(userGuess + ", ");
